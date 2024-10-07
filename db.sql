dc56e240bb8abd43687859535fa07880fd98d6a8
mrborghini <61702082+mrborghini@users.noreply.github.com>
2024-10-07 22:10:02 +0200

    first commit

---
+++ db.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE calender_project;
+
+USE calender_project;
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE tasks (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    current_status ENUM("todo", "in-progress", "done") NOT NULL,
+    due_timestamp BIGINT UNSIGNED NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NULL,
+    created_by INT NOT NULL,
+    FOREIGN KEY (created_by) REFERENCES users(id)
+);
